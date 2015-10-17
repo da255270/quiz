478e5d1f2d2f775ff0fe830e2b95d99445ec2760
da255270 <da255270@hotmail.com>
2015-10-18 00:38:56 +0800

    implement the smallest_character and the test code
    I implement the smallest_character in recursive and the test code which I default the string is "cdefjmnpsv".
    In addition,you can input any character to test the code.

--- q2/recursive.c
+++ q2/recursive.c
@@ -1,7 +1,26 @@
 /* FIXME: Implement! */
+#include <stdio.h>
+#include <assert.h>
+char smallest_character(char str[], char c,int i)
+{
+    if(str[i]=='\0')
+       return str[0];
+    if(str[i]>c)
+       return str[i];
+    smallest_character(str,c,++i);
+}
 
 int main()
 {
-    test;
+    char str[]="cdefjmnpsv";
+    char c;
+    printf("The sorted character array is %s \n",str);
+    printf("Enter the search character : ");
+    scanf("%c",&c);
+    //c = getche();
+    
+    fflush(stdin);
+    assert(printf("%c\n",smallest_character(str, c,0)));
+
     return 0;
 }
