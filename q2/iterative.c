cbcde68e9e13fca17fcf6838eb15a705e99dcbc7
da255270 <da255270@hotmail.com>
2015-10-18 00:58:24 +0800

    add computing execution time function
    add computing execution time function in diff_in_second to computing the execution time of the smallest_character function

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,5 +1,8 @@
 /* FIXME: Implement! */
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 #include <assert.h>
 char smallest_character(char str[], char c)
 {
@@ -11,18 +14,34 @@ char smallest_character(char str[], char c)
     }
     return str[0];
 }
-
+static double diff_in_second(struct timespec t1, struct timespec t2)
+{
+    struct timespec diff;
+    if (t2.tv_nsec-t1.tv_nsec < 0) {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec - 1;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + 1000000000;
+    } else {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
+    }
+    return (diff.tv_sec + diff.tv_nsec / 1000000000.0);
+}
 int main()
 {
+     struct timespec start, end;
+    double cpu_time;
     char str[]="cdefjmnpsv";
     char c;
     printf("The sorted character array is %s \n",str);
     printf("Enter the search character : ");
     scanf("%c",&c);
     //c = getche();
-    
-    fflush(stdin);
+    clock_gettime(CLOCK_REALTIME, &start);
     assert(printf("%c\n",smallest_character(str, c)));
+    clock_gettime(CLOCK_REALTIME, &end);
+    cpu_time = diff_in_second(start, end);
 
+    printf("\nexecution time of iterative : %lf sec\n", cpu_time);
+    fflush(stdin);
     return 0;
 }
