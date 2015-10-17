7cea3845ec7dd4258b1bb59a41093eead77d64dc
da255270 <da255270@hotmail.com>
2015-10-18 00:35:55 +0800

    implement the smallest_character and the test code
    I implement the smallest_character function in iteractive and the test code which I default the string is "cdefjmnpsv"

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,28 @@
 /* FIXME: Implement! */
+#include <stdio.h>
+#include <assert.h>
+char smallest_character(char str[], char c)
+{
+    int i=0;
+    while(str[i]!='\0') {
+        if(str[i]>c)
+            return str[i];
+        i++;
+    }
+    return str[0];
+}
 
 int main()
 {
+    char str[]="cdefjmnpsv";
+    char c;
+    printf("The sorted character array is %s \n",str);
+    printf("Enter the search character : ");
+    scanf("%c",&c);
+    //c = getche();
+    
+    fflush(stdin);
+    assert(printf("%c\n",smallest_character(str, c)));
+
     return 0;
 }
