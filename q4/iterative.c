e2a2bbe7dabcd35e6e93bda6e9b29e0ae4bfd991
da255270 <da255270@hotmail.com>
2015-10-18 15:54:50 +0800

    implement the maxSubArrayy and the test code
    implement the maxSubArrayy and the test code which test the execution time of the maxSubArrayy

--- q4/iterative.c
+++ q4/iterative.c
@@ -1,6 +1,49 @@
 /* FIXME: Implement! */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+static double diff_in_second(struct timespec t1, struct timespec t2)
+{
+    struct timespec diff;
+    if (t2.tv_nsec-t1.tv_nsec < 0) {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec - 1;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + 1000000000;
+    } else {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
+    }
+    return (diff.tv_sec + diff.tv_nsec / 1000000000.0);
+}
+
+int maxSubArray(int A[], int n)
+{
+    int max = A[0], sum = 0;
+    for (int i=0; i<n; ++i) {
+        sum += A[i];            
+        sum = (0 > sum ? 0 : sum);   
+
+        max = (sum > max ? sum : max);
+    }
+    return max;
+
+}
 
 int main()
 {
+    struct timespec start, end;
+    double cpu_time;
+
+    int array[9] = { -2,1,-3,4,-1,2,1,-5,4 };
+    int max;
+
+    clock_gettime(CLOCK_REALTIME, &start);
+    max = maxSubArray(array,9);
+    clock_gettime(CLOCK_REALTIME, &end);
+    cpu_time = diff_in_second(start, end);
+
+    printf("ans = %d execution time = %lf nsec\n", max, cpu_time);
+
     return 0;
 }
